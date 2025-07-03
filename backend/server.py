95502967dcb359a836d125c695fa69c0f94aa957
alfiia-bad <136993123+alfiia-bad@users.noreply.github.com>
2025-07-03 14:56:31 +0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -26,4 +26,5 @@ def not_found(e):
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render передаёт порт в этой переменной
+    app.run(host="0.0.0.0", port=port)
