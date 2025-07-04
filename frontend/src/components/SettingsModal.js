fe0f26be470a1eb6853184bd0ce7534fa163e1a3
alfiia-bad <136993123+alfiia-bad@users.noreply.github.com>
2025-07-04 18:38:29 +0500

    Update SettingsModal.js

--- frontend/src/components/SettingsModal.js
+++ frontend/src/components/SettingsModal.js
@@ -3,11 +3,20 @@ import { BiEditAlt, BiTrash } from "react-icons/bi";
 import '../index.css';
 
 export default function SettingsModal({ onClose, participants, setParticipants, currencies, setCurrencies }) {
-    useEffect(() => {
-      document.body.style.overflow = 'hidden';
-      return () => {
-        document.body.style.overflow = '';
-      };
+      useEffect(() => {
+        const scrollY = window.scrollY;
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${scrollY}px`;
+        document.body.style.width = '100%';
+        document.body.style.overflowY = 'scroll';
+    
+        return () => {
+          document.body.style.position = '';
+          document.body.style.top = '';
+          document.body.style.width = '';
+          document.body.style.overflowY = '';
+          window.scrollTo(0, scrollY);
+        }; 
     }, []);
   
   const [editingName, setEditingName] = useState(null);
